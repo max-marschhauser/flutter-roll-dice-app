999a67f797f1eea069f738e816684fcd86c93ba1
Max Marschhauser <max.marschhauser@gmail.com>
2023-05-26 10:02:41 +0200

    added basic styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello World!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 42, 11, 95),
+                Color.fromARGB(255, 103, 58, 183),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
+
+class 
