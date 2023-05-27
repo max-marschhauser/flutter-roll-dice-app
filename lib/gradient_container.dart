7a024f44a5105a8c46104e866e77a20496538fb1
max-marschhauser <max.marschhauser@gmail.com>
2023-05-27 11:56:47 +0200

    finished code

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -20,7 +20,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
+      child: Center(
         child: DiceRoller(),
       ),
     );
